9ce68c4b39801ccf0cb92562c81699abe8575e8e
Tomáš Buša <busa.tomas3@gmail.com>
2025-08-14 17:31:46 +0200

    created POST and GET for orders with changing status

--- order_class.py
+++ order_class.py
@@ -1,12 +1,14 @@
 import ulid
 class Order:
-    def __init__(self, timestamp, size, quantity):
+    def __init__(self, timestamp, size, quantity, status = 'in_progress'):
         self.id = str(ulid.new())
         self.timestamp = timestamp
         self.size = size
         self.quantity = quantity
         self.price = size * quantity * 5
-    
+        self.status = status
+    def get_id(self):
+        return self.id
     def get_timestamp(self):
         return self.timestamp
     def get_size(self):
@@ -15,5 +17,7 @@ class Order:
         return self.quantity
     def get_price(self):
         return self.price
+    def get_status(self):
+        return self.status
     
     
--- server.py
+++ server.py
@@ -5,24 +5,45 @@ import logs, order_class, quereries
 from pydantic import BaseModel
 import sqlite3
 
-
-
 app = FastAPI()
 
-try:
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-    print('DB Init!')
-    cursor.execute(quereries.create_table_query)
+def change_status(cursor, output):
+    order_id = output[0]
+    cursor.execute("UPDATE ORDERS SET STATUS = 'completed' WHERE ID = ?", (order_id, ))
+    return
+
+def manage_database(command: str, order: order_class.Order = None):
+    '''Create a connection to the database'''
+    try:
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute(quereries.create_table_query)
+        print("Connected to DB!")
+    except sqlite3.Error as error:
+        print(f"Error occured: {error}")
+
+    # add a new order to the DB
+    if command == 'POST':
+        values = [order.get_id(), order.get_timestamp(), order.get_size(), order.get_quantity(), order.get_status()]
+        cursor.execute("INSERT INTO ORDERS VALUES (?, ?, ?, ?, ?)", values)
+
+    # retrieve the first order in a queue from DB
+    if command == 'GET':
+        cursor.execute("SELECT * FROM ORDERS WHERE STATUS = 'in_progress'")
+        output = cursor.fetchone()
+        change_status(cursor, output)
+        print(output)
+    connection.commit()
     cursor.close()
-except sqlite3.Error as error:
-    print(f"Error occured: {error}")
+
+
 # get beer order
 @app.get('/order/{position}')
 def retrieve_order(position: int):
     # check whether the position is correct
     if position >= len(logs.items):
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Order not found")
+    manage_database('GET')
     return logs.items[position]
     
 
@@ -32,6 +53,7 @@ def create_order(size: int, quantity: int):
     timestamp = datetime.now()
     order = order_class.Order(timestamp, size, quantity)
     logs.items.append(order)
+    manage_database('POST', order)
     return logs.items
 
 # run the server
