573413f4f69897d078ea045bfc36ff9533f1f28d
Tomáš Buša <busa.tomas3@gmail.com>
2025-09-08 15:57:58 +0200

    need to add /ws for production in the URL

--- frontend-client/src/apiBase.ts
+++ frontend-client/src/apiBase.ts
@@ -3,6 +3,7 @@ const API = import.meta.env.VITE_API_URL || "http://localhost:8000";
 let WS: string;
 if (import.meta.env.VITE_WS_URL) {
   WS = import.meta.env.VITE_WS_URL; 
+  WS = `${WS}/ws`;  
 } else {
   WS = API.replace(/^http/, "ws"); 
   WS = WS.replace(/\/$/, "");    
