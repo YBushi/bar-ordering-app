fdc2fe29715438e95ba97b5630fd11faa760ac83
Tomáš Buša <busa.tomas3@gmail.com>
2025-09-08 15:23:52 +0200

    change the bases back to reflect the production URLs

--- frontend-client/src/apiBase.ts
+++ frontend-client/src/apiBase.ts
@@ -5,8 +5,7 @@ if (import.meta.env.VITE_WS_URL) {
   WS = import.meta.env.VITE_WS_URL; 
 } else {
   WS = API.replace(/^http/, "ws"); 
-  WS = WS.replace(/\/$/, "");    
-  WS = `${WS}/ws`;                 
+  WS = WS.replace(/\/$/, "");                     
 }
 
 export { API, WS };
